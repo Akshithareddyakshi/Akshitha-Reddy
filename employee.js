ca8e0188685f3ba6f22afe4bc1a3c08e40e2a157
Akshitha Reddyt <akshireddy1186@gmail.com>
2024-10-25 10:56:45 +0530

    javascript code completed

---
+++ employee.js
@@ -0,0 +1,11 @@
+class Employee{
+    constructor(){
+        this.id=1001;
+        this.name="akshitha reddy"
+    }
+    display(){
+        console.log(this.id+" "+this.name)
+    }
+}
+let e=new Employee();
+e.display();
